7906324ba9079d3b7a29c94b4b6f7d8108da84f7
stevent7 <stevent7@vt.edu>
2019-03-02 16:24:03 -0500

    THE version of client.py

--- client.py
+++ client.py
@@ -12,6 +12,35 @@ from PIL import Image
 from ClientKeys import ibm_watson_api_key
 from watson_developer_cloud import TextToSpeechV1
 import os
+import pickle
+import hashlib
+from cryptography.fernet import Fernet
+
+def pack_question(key, text):
+    f = Fernet(key)
+    encoded_q = text.encode('utf-8')
+    encrypted_q = f.encrypt(encoded_q)
+
+    checksum = hashlib.md5(encrypted_q).hexdigest()
+
+    unpickled_payload = (key, encrypted_q, checksum)
+
+    picked_payload = pickle.dumps(unpickled_payload)
+
+    return picked_payload
+
+def unpack_answer(key, data):
+    unpicked_payload = pickle.loads(data)
+    encrypted_a, server_checksum = unpicked_payload
+    client_checksum = hashlib.md5(encrypted_a).hexdigest()
+    if(client_checksum != server_checksum):
+        return False, "Error: did not receive the full answer."
+
+    f = Fernet(key)
+    decrypted_a = f.decrypt(encrypted_a)
+    decoded_a = decrypted_a.decode('utf-8')
+
+    return decoded_a
 
 def main(args):
 	# Label the server connection specifications
@@ -19,6 +48,8 @@ def main(args):
 	server_port = args.sp
 	socket_size = args.z
 	s = None
+
+	key = Fernet.generate_key()
 
 	# Set up the server connection
 	try:
@@ -28,14 +59,16 @@ def main(args):
 		if s:
 			s.close()
 		print ("Unable to open the socket: " + str(message))
-		# sys.exit(1)
+		sys.exit(1)
 
 	# If there is no QR Code, then decode will output: []
 	# Else there will be data, type, etc
 	# Initialize the camera stream
 	# cam = cv2.VideoCapture(0)
-	question = decode(cv2.imread('Hello_World_QR.png'))
+	# question = decode(cv2.imread('Hello_World_QR.png'))
+	question = b'What time is it?'
 
+	payload = pack_question(key, question.decode("utf-8"))
 	# print (cam.isOpened())
 
 	# initializing text-to-speech
@@ -46,6 +79,7 @@ def main(args):
 
 	# Continually scan for questions
 	while True:
+
 		# Grab a frame from the stream
 		# ret, frame = cam.read()
 		# Convert the image to grayscale
@@ -54,22 +88,20 @@ def main(args):
 		# question = decode(gray)
 
 		# If there was no readable QR code, then retry
-		if len(question) != 1:
-			continue
-		else:
-			with open('speech.wav', 'wb') as audio_file:
-				audio_file.write(
-					text_to_speech.synthesize(
-						(question[0].data).decode("utf-8"),
-						'audio/wav',
-						'en-GB_KateVoice'
-					).get_result().content)
-			os.system("omxplayer speech.wav")
-			break
-			# s.send(question.data)
 
-		# answer = s.recv(socket_size)
+		s.send(payload)
 
+		data = s.recv(socket_size)
+		answer = unpack_answer(key, data)
+		with open('speech.wav', 'wb') as audio_file:
+			audio_file.write(
+				text_to_speech.synthesize(
+					answer,
+					'audio/wav',
+					'en-GB_KateVoice'
+				).get_result().content)
+		os.system("omxplayer speech.wav")
+		break
 	# Messages should be sent in bytes b' '
 	s.close()
 
