f2b4a73bce2e1cdc808d933644c6c16bc8d81b05
Ben <bshaker@vt.edu>
2019-05-11 17:17:15 -0400

    Update ServerKeys.py

--- ServerKeys.py
+++ ServerKeys.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
 # ServerKeys.py
 
-wolframaplha_api_key = "VKK838-TX42TKG34J"
-ibm_watson_api_key = "8tY8kV6y_CR_m3Hp0-CgQdSKldyLKu0vzunGoIg37vEe"
+wolframaplha_api_key = "VKK838-TX42TKG34J" # this key has been deactivated
+ibm_watson_api_key = "8tY8kV6y_CR_m3Hp0-CgQdSKldyLKu0vzunGoIg37vEe" # this key has been deactivated
