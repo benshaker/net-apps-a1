4947c470d5b7515eaba3917fab1e621721ea7f85
Ben <bshaker@vt.edu>
2019-05-11 17:17:29 -0400

    Update ClientKeys.py

--- ClientKeys.py
+++ ClientKeys.py
@@ -1,4 +1,4 @@
 #!/usr/bin/env python3
 # ClientKeys.py
 
-ibm_watson_api_key = "8tY8kV6y_CR_m3Hp0-CgQdSKldyLKu0vzunGoIg37vEe"
+ibm_watson_api_key = "8tY8kV6y_CR_m3Hp0-CgQdSKldyLKu0vzunGoIg37vEe" # this key has been deactivated
